331a555316db06e168c115fdc119455b2ed7179a
Mani Bhushan <manib@uber.com>
2017-02-21 21:55:05 -0800

    hr designer pdf viewer

---
+++ hr_designer_pdf_viewer.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+import sys
+
+"""
+https://www.hackerrank.com/challenges/designer-pdf-viewer
+"""
+
+def main():
+    h = map(int, raw_input().strip().split(' '))
+    word = raw_input().strip()
+
+if __name__ == '__main__':
+    main()
