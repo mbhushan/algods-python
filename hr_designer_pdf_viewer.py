afa03be7e11ab0cea223ff0b480f4137d93825f8
Mani Bhushan <manib@uber.com>
2017-02-22 22:13:39 -0800

    pdf viewer highlight

--- hr_designer_pdf_viewer.py
+++ hr_designer_pdf_viewer.py
@@ -6,9 +6,20 @@ import sys
 https://www.hackerrank.com/challenges/designer-pdf-viewer
 """
 
+
 def main():
     h = map(int, raw_input().strip().split(' '))
     word = raw_input().strip()
+    alphabets = "abcdefghijklmnopqrstuvwxyz"
+    max_ht = -1 * sys.maxint
+    for s in word:
+        index = alphabets.index(s)
+        ht = h[index]
+        if ht > max_ht:
+            max_ht = ht
+    result = len(word) * max_ht
+    print result
+
 
 if __name__ == '__main__':
     main()
