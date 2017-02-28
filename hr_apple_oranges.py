7c6c5ce370ca9c72ed43821d956a4e408383a7a5
Mani Bhushan <manibhushan.cs@gmail.com>
2017-02-28 12:02:59 -0800

    apple oranges distance

--- hr_apple_oranges.py
+++ hr_apple_oranges.py
@@ -8,7 +8,27 @@ import sys
 
 
 def main():
-    pass
+    s, t = raw_input().strip().split(' ')
+    s, t = [int(s), int(t)]
+    a, b = raw_input().strip().split(' ')
+    a, b = [int(a), int(b)]
+    m, n = raw_input().strip().split(' ')
+    m, n = [int(m), int(n)]
+    apples = map(int, raw_input().strip().split(' '))
+    oranges = map(int, raw_input().strip().split(' '))
+
+    apple_op, orange_op = 0, 0
+    for app in apples:
+        x = app + a
+        if x >= s and x <= t:
+            apple_op += 1
+    for orng in oranges:
+        x = orng + b
+        if x >= s and x <= t:
+            orange_op += 1
+    print str(apple_op)
+    print str(orange_op)
+
 
 if __name__ == '__main__':
     main()
