28a4f98650946f2df46ec27a29aa9e1cfdf622fd
Mani Bhushan <manibhushan.cs@gmail.com>
2017-03-02 22:20:40 -0800

    caesar cipher

---
+++ hr_caesar_cipher.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+import sys
+
+
+def main():
+    n = int(raw_input().strip())
+    s = raw_input().strip()
+    k = int(raw_input().strip())
+
+
+if __name__ == '__main__':
+    main()
