29aa694a9004d0dadb722afa13a5b5142dcb2979
Mani Bhushan <manibhushan.cs@gmail.com>
2017-03-02 22:16:43 -0800

    pangram detection

--- hr_pangrams.py
+++ hr_pangrams.py
@@ -7,7 +7,19 @@ https://www.hackerrank.com/challenges/pangrams
 """
 
 def main():
-    pass
+    text = raw_input().strip()
+    char_set = set()
+    found = False
+    for ch in text:
+        ch = ch.lower()
+        if ch.isalpha() and ch not in char_set:
+            char_set.add(ch)
+        if len(char_set) == 26:
+            print "pangram"
+            found = True
+            break
+    if not found:
+        print "not pangram"
 
 if __name__ == '__main__':
     main()
