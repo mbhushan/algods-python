38899c11126f178842abd90600d602f8d5bb08c7
Mani Bhushan <manibhushan.cs@gmail.com>
2017-03-04 22:50:07 -0800

    mars exploration

--- mars_exploration.py
+++ mars_exploration.py
@@ -2,11 +2,22 @@
 
 """
 https://www.hackerrank.com/challenges/mars-exploration
+SOSSPSSQSSOR
+SOSOOSOSOSOSOSSOSOSOSOSOSOS
 """
 
 
 def main():
-    pass
+    S = raw_input().strip()
+    target = "SOS"
+    count = 0
+    for i in range(0, len(S)-2, 3):
+        signal = S[i: i+3]
+        if signal != target:
+            for j in range(len(target)):
+                if signal[j] != target[j]:
+                    count += 1
+    print count
 
 
 if __name__ == '__main__':
