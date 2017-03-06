06948e3e1b054070bb9d51a4747e2f13b43b6408
Mani Bhushan <manibhushan.cs@gmail.com>
2017-03-05 21:43:39 -0800

    funny string - init commit

---
+++ hr_funny_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys
+
+"""
+https://www.hackerrank.com/challenges/funny-string
+"""
+
+def main():
+    S = raw_input().strip()
+
+if __name__ == '__main__':
+    main()
