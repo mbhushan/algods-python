c6b090093a21d12be3520eacbad205898ab08d43
Mani Bhushan <manibhushan.cs@gmail.com>
2017-03-07 21:57:40 -0800

    funny string - implementation

--- hr_funny_string.py
+++ hr_funny_string.py
@@ -7,8 +7,21 @@ https://www.hackerrank.com/challenges/funny-string
 """
 
 def main():
-    S = raw_input().strip()
-    R = S[::-1]
+    T = int(raw_input().strip())
+    for i in range(T):
+        S = raw_input().strip()
+        R = S[::-1]
+        funny = True
+        n = len(S)
+        for j in range(1, n):
+            if abs(ord(S[j]) - ord(S[j-1])) != abs(ord(R[j]) - ord(R[j-1])):
+                funny = False
+                break
+        if funny:
+            print "Funny"
+        else:
+            print "Not Funny"
+
 
 if __name__ == '__main__':
     main()
