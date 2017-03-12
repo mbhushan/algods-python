6b5fedf6d6e54313ad9f9f9766bd367d6e1e35b9
Mani Bhushan <manibhushan.cs@gmail.com>
2017-03-11 22:02:50 -0800

    weighted strings

--- hr_weighted_uniform_strings.py
+++ hr_weighted_uniform_strings.py
@@ -1,12 +1,18 @@
 #!/bin/python
 
+import sys
+
 """
 https://www.hackerrank.com/challenges/weighted-uniform-string
 """
 
 
 def main():
-    pass
+    s = raw_input().strip()
+    n = int(raw_input().strip())
+    for a0 in xrange(n):
+        x = int(raw_input().strip())
+        # your code goes here
 
 if __name__ == '__main__':
     main()
