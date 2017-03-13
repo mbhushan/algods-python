229a16a37bfbe2ed0f47e4bf9addb76b81d0a460
Mani Bhushan <manibhushan.cs@gmail.com>
2017-03-12 21:36:59 -0700

    weighted strings

--- hr_weighted_uniform_strings.py
+++ hr_weighted_uniform_strings.py
@@ -6,6 +6,9 @@ import sys
 https://www.hackerrank.com/challenges/weighted-uniform-string
 """
 
+def factors(n):
+    return set(reduce(list.__add__,
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
 
 def main():
     s = raw_input().strip()
