7a070de3d1132e17c0766b28d97b7af2acdb7b19
Mani Bhushan <manibhushan.cs@gmail.com>
2017-03-14 22:32:16 -0700

    weighted strings

--- hr_weighted_uniform_strings.py
+++ hr_weighted_uniform_strings.py
@@ -8,8 +8,14 @@ https://www.hackerrank.com/challenges/weighted-uniform-string
 
 
 def factors(n):
+    """
+    Returns the factors of a number n
+    :param n:
+    :return:
+    """
     return set(reduce(list.__add__,
                 ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
 
 def check_weight(s, x):
     fs = factors(x)
