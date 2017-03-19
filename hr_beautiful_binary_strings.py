cef88d0bd4b6f275b8cb09a68d5a1ab26a110f70
manibhushan <manibhushan.cs@gmail.com>
2017-03-18 22:52:17 -0700

    beautiful binary strings

---
+++ hr_beautiful_binary_strings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2.7
+
+import sys
+
+"""
+https://www.hackerrank.com/challenges/beautiful-binary-string
+"""
+
+
+def main():
+    n = int(raw_input().strip())
+    B = raw_input().strip()
+
+if __name__ == '__main__':
+    main()
