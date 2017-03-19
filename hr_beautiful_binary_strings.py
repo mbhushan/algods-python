160fbfa5150824e9284d8ccf49808007181e9cf4
manibhushan <manibhushan.cs@gmail.com>
2017-03-18 23:00:52 -0700

    beautiful binary strings

--- hr_beautiful_binary_strings.py
+++ hr_beautiful_binary_strings.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 
 import sys
+import re
 
 """
 https://www.hackerrank.com/challenges/beautiful-binary-string
@@ -10,6 +11,10 @@ https://www.hackerrank.com/challenges/beautiful-binary-string
 def main():
     n = int(raw_input().strip())
     B = raw_input().strip()
+    pat = "010"
+    indices = [m.start() for m in re.finditer(pat, B)]
+    # print "{0}".format(indices)
+    print "{}".format(len(indices))
 
 if __name__ == '__main__':
     main()
