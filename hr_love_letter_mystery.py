68635bfb3ef85626d7d17862621d5f77c1b16c45
manibhushan <manibhushan.cs@gmail.com>
2017-03-19 22:45:24 -0700

    love letter mystery

---
+++ hr_love_letter_mystery.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2.7
+
+import sys
+
+"""
+https://www.hackerrank.com/challenges/the-love-letter-mystery
+"""
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
