ed31c6bd8f664efb668eaf872e1cdf614211c1af
manibhushan <manibhushan.cs@gmail.com>
2017-03-21 22:02:08 -0700

    hr love letter solution

--- hr_love_letter_mystery.py
+++ hr_love_letter_mystery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 
 import sys
+import math
 
 """
 https://www.hackerrank.com/challenges/the-love-letter-mystery
@@ -9,7 +10,19 @@ https://www.hackerrank.com/challenges/the-love-letter-mystery
 
 def main():
     T = int(raw_input().strip())
-    S = raw_input().strip()
+    for i in range(T):
+        S = raw_input().strip()
+        value = 0
+        j = 0
+        k = len(S) - 1
+        while (j < k):
+            value += abs(ord(S[j]) - ord(S[k]))
+            j = j + 1
+            k = k - 1
+        print value
+
+
+
 
 if __name__ == '__main__':
     main()
