70d738b209bebbcb60ac903952ea27bec3d0b41b
manibhushan <manibhushan.cs@gmail.com>
2017-03-23 22:27:21 -0700

    gem stones

--- hr_gem_stones.py
+++ hr_gem_stones.py
@@ -7,7 +7,13 @@ https://www.hackerrank.com/challenges/gem-stones
 """
 
 def main():
-    pass
+    T = int(raw_input().strip())
+    S = raw_input().strip()
+    for i in range(T-1):
+        T = raw_input().strip()
+        S = [val for val in T if val in S]
+    print "{}".format(len(S))
+
 
 if __name__ == '__main__':
     main()
