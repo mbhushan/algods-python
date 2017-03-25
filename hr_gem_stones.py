c5586e43949d9075fe0d9bb3d8cf5a631ac90c05
manibhushan <manibhushan.cs@gmail.com>
2017-03-24 22:23:47 -0700

    gem stones

--- hr_gem_stones.py
+++ hr_gem_stones.py
@@ -12,7 +12,7 @@ def main():
     for i in range(T-1):
         T = raw_input().strip()
         S = [val for val in T if val in S]
-    print "{}".format(len(S))
+    print "{}".format(len(set(S)))
 
 
 if __name__ == '__main__':
