683b8ed9c6e68bb07a6dcea4054159af016fd8d3
Mani Bhushan <manibhushan.cs@gmail.com>
2017-03-27 21:51:09 -0700

    hr anagrams - first checking

---
+++ hr_anagrams.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+
+"""
+https://www.hackerrank.com/challenges/anagram
+"""
+
+
+def main():
+    T = int(raw_input().strip())
+    for i in range(T):
+        S = raw_input().strip()
+
+if __name__ == '__main__':
+    main()
