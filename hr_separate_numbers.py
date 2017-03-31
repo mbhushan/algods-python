7dfd7fc3cc8764ef140893fb4397229f373ec2e1
Mani Bhushan <manibhushan.cs@gmail.com>
2017-03-30 22:28:27 -0700

    intial commit separate numbers

---
+++ hr_separate_numbers.py
@@ -0,0 +1,17 @@
+#!/bin/python
+
+import sys
+
+"""
+https://www.hackerrank.com/challenges/separate-the-numbers
+"""
+
+
+def main():
+    q = int(raw_input().strip())
+    for a0 in xrange(q):
+        s = raw_input().strip()
+        # your code goes here
+
+if __name__ == '__main__':
+    main()
