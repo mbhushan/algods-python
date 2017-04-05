df5d84f0680973dc0bb858bc87a524a631afd9c9
manibhushan <manibhushan.cs@gmail.com>
2017-04-04 21:37:45 -0700

    game of thrones - init checkin

---
+++ hr_game_of_thrones.py
@@ -0,0 +1,21 @@
+import sys
+
+
+"""
+https://www.hackerrank.com/challenges/game-of-thrones
+"""
+
+def main():
+    string = raw_input()
+
+    found = False
+    # Write the code to find the required palindrome and then assign the variable 'found' a value of True or False
+
+    if not found:
+        print("NO")
+    else:
+        print("YES")
+
+
+if __name__ == '__main__':
+    main()
