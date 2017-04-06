105fd5ffa7afd01e6cff1dfc5ac3bf67995d7cb9
Mani Bhushan <manibhushan.cs@gmail.com>
2017-04-05 22:04:48 -0700

    hackerank string

---
+++ hr_hackerrank_string.py
@@ -0,0 +1,18 @@
+#!/bin/python
+
+import sys
+
+
+"""
+https://www.hackerrank.com/challenges/hackerrank-in-a-string
+"""
+
+
+def main():
+    q = int(raw_input().strip())
+    for a0 in xrange(q):
+        s = raw_input().strip()
+        # your code goes here
+
+if __name__ == '__main__':
+    main()
